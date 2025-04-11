c7d03e1d68ab59200aa7522e74d20d2a4bb791a6
Auto <alswl1937@naver.com>
2025-04-11 14:28:36 +0900

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,10 @@ export const metadata = {
   description: 'Calendar for managing patient tags',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
 }
-
